84da80569a55e536b0ad299ba93c233684b1e672
Kurok00 <heneken96@gmail.com>
2024-12-24 14:45:38 +0700

    ll

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,18 +1,18 @@
-
 module.exports = {
-    "env": {
-        "node": true,
-        "es2021": true,
-        "jest": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-        "indent": ["error", 2],
-        "quotes": ["error", "single"],
-        "semi": ["error", "always"]
-    }
+  'env': {
+    'node': true,
+    'es2021': true,
+    'jest': true
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'rules': {
+    'indent': ['error', 2],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'no-unused-vars': ['error', { 'args': 'none' }]
+  }
 };
