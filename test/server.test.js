a6c55d86860d6a28d9c8003eaec5950b42a81953
Kurok00 <heneken96@gmail.com>
2024-12-24 10:43:52 +0700

    Initial commit

---
+++ test/server.test.js
@@ -0,0 +1,11 @@
+
+const request = require('supertest');
+const app = require('../server');
+
+describe('GET /', () => {
+  it('should return Hello World message', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toBe(200);
+    expect(res.body.message).toBe('Hello World!');
+  });
+});
